19fb161dbd192090932713176128744b4a123bf1
Clodoaldo35 <157506115+Clodoaldo35@users.noreply.github.com>
2025-03-08 22:54:03 -0300

    Initial commit: Configuração inicial do projeto Django
    Adiciona arquivos e pastas iniciais do projeto Django,
    incluindo configurações, URLs, modelos e arquivos de gerenciamento.

---
+++ products/admin.py
@@ -0,0 +1,41 @@
+import csv
+from django.http import HttpResponse
+from django.contrib import admin
+from .models import Brand, Category, Product    
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active', 'description', 'created_at', 'updated_at']
+    search_fields = ['name']
+    list_filter = ['is_active']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active', 'description', 'created_at', 'updated_at']
+    search_fields = ['name']
+    list_filter = ['is_active']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'brand', 'category', 'price', 'stock', 'is_active', 'description', 'created_at', 'updated_at']
+    search_fields = ['title', 'brand__name', 'category__name']
+    list_filter = ['is_active', 'brand', 'category']
+
+# list_editable = ('stock',) # remova caso tenha descomentado anteriormente
+
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        if not obj:  # Adiciona 'stock' apenas no formulário de criação
+            return fields + ('stock',)
+        return fields
+
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="products.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['titulo', 'marca', 'categoria', 'preço', 'estoque', 'ativo', 'descrição', 'criado em', 'atualizado em'])
+        for product in queryset:
+            writer.writerow([product.title, product.brand.name, product.category.name, product.price, product.stock, product.is_active, product.description, product.created_at, product.updated_at])
+        return response
+    export_to_csv.short_description = 'Exportar para CSV'
+    actions = ['export_to_csv']          
