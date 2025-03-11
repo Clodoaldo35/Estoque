803ccdc636f3b81430d72262cade5be29fe95aad
Clodoaldo35 <157506115+Clodoaldo35@users.noreply.github.com>
2025-03-10 21:42:42 -0300

    fazer alterações no arquivo settings.py

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,5 @@ jazzmin_settings = {
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "C & P",
 
-    "icons": {
-        "products.Product": "fas fa-box",
-    },
+   
 }
