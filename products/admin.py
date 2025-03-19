7208d1f97a7d79d3c3b2062a8bac635d84d90f02
Clodoaldo35 <157506115+Clodoaldo35@users.noreply.github.com>
2025-03-19 18:11:43 -0300

    Fazendo alterações no arquivo products/admin.py

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from django.contrib import admin
 from .models import Brand, Category, Product
 
+
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ['name', 'is_active', 'description', 'created_at', 'updated_at']
+    list_display = [
+        'name', 'is_active', 'description', 'created_at', 'updated_at'
+    ]
     search_fields = ['name']
     list_filter = ['is_active']
 
@@ -21,7 +24,9 @@ class BrandAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'is_active', 'description', 'created_at', 'updated_at']
+    list_display = [
+        'name', 'is_active', 'description', 'created_at', 'updated_at'
+    ]
     search_fields = ['name']
     list_filter = ['is_active']
 
@@ -37,7 +42,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'brand', 'category', 'price', 'stock', 'is_active', 'description', 'created_at', 'updated_at']
+    list_display = [
+        'title', 'brand', 'category', 'price', 'stock',
+        'is_active', 'description', 'created_at', 'updated_at'
+    ]
     search_fields = ['title', 'brand__name', 'category__name']
     list_filter = ['is_active', 'brand', 'category']
     list_editable = ('stock',)
@@ -54,17 +62,32 @@ class ProductAdmin(admin.ModelAdmin):
     updated_at.short_description = 'Atualizado em'
 
     def get_fields(self, request, obj=None):
-        fields = list(super().get_fields(request, obj)) # Convertendo para lista
+        fields = list(
+            super().get_fields(request, obj)
+        )  # Convertendo para lista
         if not obj:  # Adiciona 'stock' apenas no formulário de criação
-            fields.append('stock') # Usando append para adicionar à lista
+            fields.append('stock')  # Usando append para adicionar à lista
         return fields
 
     def export_to_csv(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="products.csv"'
         writer = csv.writer(response)
-        writer.writerow(['Titulo', 'Marca', 'Categoria', 'Preço', 'Estoque', 'Ativo', 'Descrição', 'Criado em', 'Atualizado em'])
+        writer.writerow([
+            'Titulo', 'Marca', 'Categoria', 'Preço', 'Estoque', 'Ativo',
+            'Descrição', 'Criado em', 'Atualizado em'
+        ])
         for product in queryset:
-            writer.writerow([product.title, product.brand.name, product.category.name, product.price, product.stock, product.is_active, product.description, product.created_at.strftime('%d-%m-%Y %H:%M'), product.updated_at.strftime('%d-%m-%Y %H:%M')])
+            writer.writerow([
+                product.title,
+                product.brand.name,
+                product.category.name,
+                product.price,
+                product.stock,
+                product.is_active,
+                product.description,
+                product.created_at.strftime('%d-%m-%Y %H:%M'),
+                product.updated_at.strftime('%d-%m-%Y %H:%M')
+            ])
         return response
     export_to_csv.short_description = 'Exportar para CSV'
